4db9c2a36fccfc090a3d04748b96cb38cb276438
RONNIE MIKIHIRO SATO LOPES <30299318+ronniemikihiro@users.noreply.github.com>
2020-11-25 17:57:09 -0300

    Adicionando inserção de credenciais oauth2 para requisição.

--- rest-auth/src/main/java/com/example/rest/auth/swagger/SwaggerConfig.java
+++ rest-auth/src/main/java/com/example/rest/auth/swagger/SwaggerConfig.java
@@ -1,13 +1,17 @@
 package com.example.rest.auth.swagger;
 
+import com.google.common.collect.Lists;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.OAuthBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.ApiKeyVehicle;
+import springfox.documentation.swagger.web.SecurityConfiguration;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
@@ -19,10 +23,12 @@ public class SwaggerConfig {
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.example.rest.auth.controller"))
                 .build()
-                .apiInfo(metaData());
+                .apiInfo(apiInfo())
+                .securitySchemes(Lists.newArrayList(securityScheme()))
+                .securityContexts(Lists.newArrayList(securityContext()));
     }
 
-    private ApiInfo metaData() {
+    private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
                 .title("Spring Boot Microservices")
                 .description("Example Microservices")
@@ -31,5 +37,31 @@ public class SwaggerConfig {
                 .license("Private for esales")
                 .licenseUrl("https://esales.com.br/")
                 .build();
+    }
+    
+    @Bean
+    public SecurityConfiguration securityInfo() {
+        return new SecurityConfiguration(CLIENT_ID, CLIENT_SECRET, "", "", "", ApiKeyVehicle.HEADER, "", " ");
+    }
+
+
+    private SecurityScheme securityScheme() {
+        return new OAuthBuilder().name(SECURITY_SCHEME_NAME)
+                .grantTypes(Lists.newArrayList(new ResourceOwnerPasswordCredentialsGrant(AUTH_SERVER + "/oauth/token")))
+                .scopes(Lists.newArrayList(scopes()))
+                .build();
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder()
+                .securityReferences(Lists.newArrayList(new SecurityReference(SECURITY_SCHEME_NAME, scopes())))
+                .build();
+    }
+
+    private AuthorizationScope[] scopes() {
+        return new AuthorizationScope[] {
+            new AuthorizationScope("read", "ler todos"),
+            new AuthorizationScope("write", "acesso total")
+        };
     }
 }
